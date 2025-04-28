21363d3b5498981471878146d3bcb4b8d3ad2504
90DegreeHunchBack <et.we319@gmail.com>
2025-04-28 18:00:16 -0400

    done

--- main.code.py
+++ main.code.py
@@ -1,6 +1,8 @@
 name = input("enter name")
 print(" you start in a dark room and suddenly the lights turn on, then you saw a shadow, he introdused himeself as diddy")
-print(f" hello there {name} he said")
-print("HOW DA HAIL DO YOU KNOW MY NAME")
-print("I KNOW EVERYTHING")
-
+print(f" Diddy: hello there {name}...")
+print("You: H...how do you know my name!")
+print("Diddy: I KNOW EVERYTHING...")
+[print("{PORTAL APPEARS}")
+print("You: (gasp)!")
+print(YOU HAVE NOW ENETERED THE DUNGEON)
